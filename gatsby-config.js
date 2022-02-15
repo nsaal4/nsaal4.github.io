449633e213aa9324d87e5c1275cd3265a8078820
nsaal4 <ibnabbeyd@gmail.com>
2022-02-15 22:33:43 +0300

    Modify gatsby-config.js file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
-    options: {
-      // "icon": "src/images/icon.png"
-    }
+    // options: {
+    //   "icon": "src/images/icon.png"
+    // }
   }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
