850c3ba6e6247524989470621c1ed516a0ee301b
nsaal4 <ibnabbeyd@gmail.com>
2022-02-15 21:10:13 +0300

    Modify gatsby-config.js file

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,8 @@ module.exports = {
       title: `my_portfolio`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
