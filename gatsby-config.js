78dba65f88b336614df9703dd4793748c5002f2d
nsaal4 <ibnabbeyd@gmail.com>
2022-02-15 22:32:14 +0300

    Modify gatsby-config.js file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   siteMetadata: {
-      title: `my_portfolio`,
+      title: `Nour Abeid | Portfolio`,
     siteUrl: `https://www.nsaal4.github.io`
   },
   plugins: [
     "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      // "icon": "src/images/icon.png"
     }
   }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
