187a17e0be6050dc59b326c2be8a7c112cfef1a1
nsaal4 <ibnabbeyd@gmail.com>
2022-02-15 22:34:54 +0300

    Modify gatsby-config.js file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,13 @@ module.exports = {
     siteUrl: `https://www.nsaal4.github.io`
   },
   plugins: [
-    "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    // options: {
-    //   "icon": "src/images/icon.png"
-    // }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  //   "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  //   resolve: 'gatsby-plugin-manifest',
+  //   options: {
+  //     "icon": "src/images/icon.png"
+  //   }
+  // },
+    "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
